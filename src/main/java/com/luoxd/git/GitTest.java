9c2038f4204b4f8cdc8fc3c53cd106c5b4bbee8b
Gitluoxd <1544824203@qq.com>
2022-10-03 17:37:54 +0800

    second commit

--- src/main/java/com/luoxd/git/GitTest.java
+++ src/main/java/com/luoxd/git/GitTest.java
@@ -8,5 +8,6 @@ package com.luoxd.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
