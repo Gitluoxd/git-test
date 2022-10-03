255b5f25ad3725b362db098b2b6c979fe7d86751
Gitluoxd <1544824203@qq.com>
2022-10-03 17:35:58 +0800

    first commit

---
+++ src/main/java/com/luoxd/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.luoxd.git;
+
+/**
+ * @author l
+ * @date 2022/10/3 0003 17:31
+ * @Remark
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
